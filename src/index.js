7f127a87d2dc40c238944e24e24c9ecfc44ae7e9
Andrea Parodi <andrea@parro.it>
2015-11-13 17:50:42 +0100

    frist commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,70 @@
-export default function niceDialogs() {
-  return 42;
-}
+import { readFileSync } from 'fs';
+
+const alertTemplate = readFileSync(`${__dirname}/alert-template.html`);
+const confirmTemplate = readFileSync(`${__dirname}/confirm-template.html`);
+
+module.exports = {
+  _createElm() {
+    const placeHolder = document.createElement('div');
+    placeHolder.innerHTML = alertTemplate;
+    this._elm = placeHolder.children[0];
+    document.body.appendChild(this._elm);
+    return this._elm;
+  },
+  _createConfirmElm() {
+    const placeHolder = document.createElement('div');
+    placeHolder.innerHTML = confirmTemplate;
+    this._confirmElm = placeHolder.children[0];
+    document.body.appendChild(this._confirmElm);
+    return this._confirmElm;
+  },
+
+
+  alert(message, title) {
+    const alertElm = this._elm ? this._elm : this._createElm();
+    alertElm.querySelector('.title').innerHTML = title;
+    alertElm.querySelector('main').innerHTML = message;
+
+    const btnOk = alertElm.querySelector('.ok');
+
+    return new Promise(resolve => {
+      const onOk = () => {
+        alertElm.close(true);
+        resolve();
+        btnOk.removeEventListener('click', onOk);
+      };
+
+      btnOk.addEventListener('click', onOk);
+      alertElm.showModal();
+    });
+  },
+
+  confirm(message, title) {
+    const confirmElm = this._confirmElm ? this._confirmElm : this._createConfirmElm();
+    confirmElm.querySelector('.title').innerHTML = title;
+    confirmElm.querySelector('main').innerHTML = message;
+
+    const btnYes = confirmElm.querySelector('.yes');
+    const btnNo = confirmElm.querySelector('.no');
+
+    return new Promise(resolve => {
+      const onYes = () => {
+        confirmElm.close(true);
+        resolve(true);
+        btnYes.removeEventListener('click', onYes);
+        btnNo.removeEventListener('click', onNo); // eslint-disable-line
+      };
+
+      const onNo = () => {
+        confirmElm.close(false);
+        resolve(false);
+        btnYes.removeEventListener('click', onYes);
+        btnNo.removeEventListener('click', onNo);
+      };
+
+      btnYes.addEventListener('click', onYes);
+      btnNo.addEventListener('click', onNo);
+      confirmElm.showModal();
+    });
+  }
+};
--- test.js
+++ test.js
@@ -1,8 +1,26 @@
 import test from 'tape';
-import niceDialogs from './src';
+// import niceDialogs from './src';
+import nightmare from 'nightmare';
 
 test('it work!', t => {
-  const result = niceDialogs();
-  t.equal(result, 42);
+  // const result = niceDialogs();
+
+  nightmare()
+    .goto('http://yahoo.com')
+    .type('input[title="Search"]', 'github nightmare')
+    .click('.searchsubmit');
+
+  // t.equal(result, 42);
   t.end();
 });
+
+/*
+app.commands.register('alert', co.wrap(function * () {
+    yield app.dialogs.alert('This is the alert message', 'Info');
+    alert('message closed');
+  }));
+  app.commands.register('confirm', co.wrap(function * () {
+    const answer = yield app.dialogs.confirm('Are you really sure?', 'Question');
+    alert('answer ' + answer);
+  }));
+*/
