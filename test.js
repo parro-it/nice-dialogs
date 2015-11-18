312f7b9ec3a84af8abdeb7931f33efe84077b747
Andrea Parodi <andrea@parro.it>
2015-11-18 08:22:58 +0100

    confirm test

--- test.js
+++ test.js
@@ -10,16 +10,26 @@ function createNightmare() {
       preload: `${__dirname}/test-preload.js`,
       'web-security': false
     }
-  });
+  }).goto('about://blank');
 }
 
 const mkUtils = (dialogSelector, nightmare) => ({
   openAlert() {
     return nightmare
-      .goto('about://blank')
       .evaluate(() => {
         const msg = 'This is the alert message';
         window.dialogs.alert(msg, 'Info').then( v => {
+          window._result = v;
+        });
+      })
+      .wait(dialogSelector);
+  },
+
+  openConfirm() {
+    return nightmare
+      .evaluate(() => {
+        const msg = 'This is the confirm message';
+        window.dialogs.confirm(msg, 'Question').then( v => {
           window._result = v;
         });
       })
@@ -46,28 +56,65 @@ const mkUtils = (dialogSelector, nightmare) => ({
 
 const nightmare = createNightmare();
 
-test('it work!', co.wrap(function * (t) {
+test('alert', co.wrap(function * (t) {
   const utils = mkUtils('#alert-dialog', nightmare);
 
-  yield utils.openAlert(nightmare);
+  yield utils.openAlert();
 
   t.ok(nightmare.visible('#alert-dialog main'));
 
-  t.ok(yield utils.dialogIsOpened(nightmare));
-  t.equal('This is the alert message', yield utils.getMessage(nightmare));
+  t.ok(yield utils.dialogIsOpened());
+  t.equal('This is the alert message', yield utils.getMessage());
 
-  t.equal('Info', yield utils.getTitle(nightmare));
+  t.equal('Info', yield utils.getTitle());
 
   yield nightmare
     .click('#alert-dialog .ok');
 
-  t.ok(!(yield utils.dialogIsOpened(nightmare)));
+  t.ok(!(yield utils.dialogIsOpened()));
   t.ok(yield utils.resolvedTo(true));
 
 
+  t.end();
+}));
+
+test('confirm - yes', co.wrap(function * (t) {
+  const utils = mkUtils('#confirm-dialog', nightmare);
+  yield utils.openConfirm();
+  t.ok(nightmare.visible('#confirm-dialog main'));
+
+  t.ok(yield utils.dialogIsOpened());
+  t.equal('This is the confirm message', yield utils.getMessage());
+
+  t.equal('Question', yield utils.getTitle());
+
+  yield nightmare
+    .click('#confirm-dialog .yes');
+
+  t.ok(!(yield utils.dialogIsOpened()));
+  t.ok(yield utils.resolvedTo(true));
+
+  t.end();
+}));
+
+test('confirm - no', co.wrap(function * (t) {
+  const utils = mkUtils('#confirm-dialog', nightmare);
+  yield utils.openConfirm();
+
+  yield nightmare
+    .click('#confirm-dialog .no');
+
+  t.ok(yield utils.resolvedTo(false));
+
+  t.end();
+}));
+
+
+test('awake', co.wrap(function * (t) {
   yield nightmare.end();
   t.end();
 }));
+
 
 /*
 
