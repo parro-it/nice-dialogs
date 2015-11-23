daa360144686dcf3efb75348f3e6413357f928cf
Andrea Parodi <andrea@parro.it>
2015-11-23 09:11:16 +0100

    refactored

--- src/index.js
+++ src/index.js
@@ -2,44 +2,69 @@ const readFileSync = require('fs').readFileSync;
 const alertTemplate = readFileSync(`${__dirname}/alert-template.html`);
 const confirmTemplate = readFileSync(`${__dirname}/confirm-template.html`);
 
+function createElement(template) {
+  const placeHolder = document.createElement('div');
+  placeHolder.innerHTML = template;
+  const elm = placeHolder.children[0];
+  document.body.appendChild(elm);
+  return elm;
+}
+
+function dialog(dialogElm, resolve) {
+  const buttons = [];
+
+  function clear() {
+    for (const btn of buttons) {
+      btn.elm.removeEventListener('click', btn.onClick);
+    }
+  }
+
+  return (elm, result) => {
+    const onClick = () => {
+      dialogElm.close(result);
+      resolve(result);
+      clear();
+    };
+
+    elm.addEventListener('click', onClick);
+    buttons.push({
+      elm: elm,
+      onClick: onClick
+    });
+  };
+}
+
 module.exports = {
   _createElm() {
-    const placeHolder = document.createElement('div');
-    placeHolder.innerHTML = alertTemplate;
-    this._elm = placeHolder.children[0];
-    document.body.appendChild(this._elm);
+    if (!this._elm) {
+      this._elm = createElement(alertTemplate);
+    }
     return this._elm;
   },
+
   _createConfirmElm() {
-    const placeHolder = document.createElement('div');
-    placeHolder.innerHTML = confirmTemplate;
-    this._confirmElm = placeHolder.children[0];
-    document.body.appendChild(this._confirmElm);
+    if (!this._confirmElm) {
+      this._confirmElm = createElement(confirmTemplate);
+    }
     return this._confirmElm;
   },
 
-
   alert(message, title) {
-    const alertElm = this._elm ? this._elm : this._createElm();
+    const alertElm = this._createElm();
     alertElm.querySelector('.title').innerHTML = title;
     alertElm.querySelector('main').innerHTML = message;
 
     const btnOk = alertElm.querySelector('.ok');
 
     return new Promise(resolve => {
-      const onOk = () => {
-        alertElm.close(true);
-        resolve(true);
-        btnOk.removeEventListener('click', onOk);
-      };
-
-      btnOk.addEventListener('click', onOk);
+      const dlg = dialog(alertElm, resolve);
+      dlg(btnOk, true);
       alertElm.showModal();
     });
   },
 
   confirm(message, title) {
-    const confirmElm = this._confirmElm ? this._confirmElm : this._createConfirmElm();
+    const confirmElm = this._createConfirmElm();
     confirmElm.querySelector('.title').innerHTML = title;
     confirmElm.querySelector('main').innerHTML = message;
 
@@ -47,22 +72,11 @@ module.exports = {
     const btnNo = confirmElm.querySelector('.no');
 
     return new Promise(resolve => {
-      const onYes = () => {
-        confirmElm.close(true);
-        resolve(true);
-        btnYes.removeEventListener('click', onYes);
-        btnNo.removeEventListener('click', onNo); // eslint-disable-line
-      };
+      const dlg = dialog(confirmElm, resolve);
+      dlg(btnNo, false);
+      dlg(btnYes, true);
 
-      const onNo = () => {
-        confirmElm.close(false);
-        resolve(false);
-        btnYes.removeEventListener('click', onYes);
-        btnNo.removeEventListener('click', onNo);
-      };
 
-      btnYes.addEventListener('click', onYes);
-      btnNo.addEventListener('click', onNo);
       confirmElm.showModal();
     });
   }
